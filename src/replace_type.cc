c747b6574a5a8851a21d96443a31d0d62fd0a292
Lewin Bormann <lbo@spheniscida.de>
2020-03-25 13:12:14 +0100

    replace types: with mpl::if_

--- src/replace_type.cc
+++ src/replace_type.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <type_traits>
+#include <boost/mpl/if.hpp>
+
+namespace mpl = boost::mpl;
 
 template<typename C, typename X, typename Y>
 struct replace_type {
@@ -46,22 +49,26 @@ namespace {
 
 template<typename C, typename X, typename Y>
 struct replace_type<C*, X, Y> {
-    typedef typename replace_type_ptr_helper<std::is_same<C*, X>::value, C*, X, Y>::type type;
+    //typedef typename replace_type_ptr_helper<std::is_same<C*, X>::value, C*, X, Y>::type type;
+    typedef typename mpl::if_<std::is_same<C*, X>, Y, typename replace_type<C, X, Y>::type*>::type type;
 };
 
 template<typename C, typename X, typename Y>
 struct replace_type<C&, X, Y> {
-    typedef typename replace_type_ptr_helper<std::is_same<C&, X>::value, C&, X, Y>::type type;
+    //typedef typename replace_type_ptr_helper<std::is_same<C&, X>::value, C&, X, Y>::type type;
+    typedef typename mpl::if_<std::is_same<C&, X>, Y, typename replace_type<C, X, Y>::type&>::type type;
 };
 
 template<typename C, typename X, typename Y>
 struct replace_type<C&&, X, Y> {
-    typedef typename replace_type_ptr_helper<std::is_same<C&&, X>::value, C&&, X, Y>::type type;
+    //typedef typename replace_type_ptr_helper<std::is_same<C&&, X>::value, C&&, X, Y>::type type;
+    typedef typename mpl::if_<std::is_same<C&&, X>, Y, typename replace_type<C, X, Y>::type&&>::type type;
 };
 
 template<typename C, typename X, typename Y>
 struct replace_type<const C, X, Y> {
-    typedef const typename replace_type_ptr_helper<std::is_same<const C, X>::value, const C, X, Y>::type type;
+    //typedef const typename replace_type_ptr_helper<std::is_same<const C, X>::value, const C, X, Y>::type type;
+    typedef typename mpl::if_<std::is_same<const C, X>, Y, const typename replace_type<C, X, Y>::type>::type type;
 };
 
 template<typename R, typename A, typename X, typename Y>
@@ -70,7 +77,7 @@ struct replace_type<R (*)(A), X, Y> {
 };
 
 int main(void) {
-    typedef const long long* typ;
+    typedef const long long& (*typ)(long long&&);
     std::cout << typeid(typ).name() << std::endl;
     std::cout << typeid(replace_type<typ, long long, unsigned>::type).name() << std::endl;
     return 0;
